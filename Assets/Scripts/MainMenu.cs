27a0d55167e142130acd716621617f17e79518b3
Loren <lorenal28102002@gmail.com>
2025-05-16 12:44:40 +0300

    Adding session list

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void OptionsMenu()
+    // This function loads the next scene in the build index
+    public void OptionMenu()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // This function quits the application
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
 }
